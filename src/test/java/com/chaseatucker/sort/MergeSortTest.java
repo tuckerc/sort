89c3255dca969b0c263fec6380723eb93f392b4e
Chase Tucker <tuck8106@gmail.com>
2020-07-30 12:28:14 -0700

    mergesort implemented and tested

---
+++ src/test/java/com/chaseatucker/sort/MergeSortTest.java
@@ -0,0 +1,80 @@
+package com.chaseatucker.sort;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class MergeSortTest {
+
+  List<Integer> numbers;
+  List<Integer> numbersTrustedSort;
+
+  @Test
+  public void sortSmallHappyTest() {
+    int size = 10;
+    numbers = newArrayList();
+    for (int i = 0; i < size; i++) {
+      numbers.add((int) Math.floor(Math.random() * size));
+    }
+    numbersTrustedSort = newArrayList(numbers);
+    Collections.sort(numbersTrustedSort);
+    MergeSort.sort(numbers);
+    assertEquals(numbersTrustedSort, numbers);
+  }
+
+  @Test
+  public void sortMediumHappyTest() {
+    int size = 1000;
+    numbers = newArrayList();
+    for (int i = 0; i < size; i++) {
+      numbers.add((int) Math.floor(Math.random() * size));
+    }
+    numbersTrustedSort = newArrayList(numbers);
+    Collections.sort(numbersTrustedSort);
+    MergeSort.sort(numbers);
+    assertEquals(numbersTrustedSort, numbers);
+  }
+
+  @Test
+  public void sortLargeHappyTest() {
+    int size = 100000;
+    numbers = newArrayList();
+    for (int i = 0; i < size; i++) {
+      numbers.add((int) Math.floor(Math.random() * size));
+    }
+    numbersTrustedSort = newArrayList(numbers);
+    Collections.sort(numbersTrustedSort);
+    MergeSort.sort(numbers);
+    assertEquals(numbersTrustedSort, numbers);
+  }
+
+  @Test
+  public void sortXLHappyTest() {
+    int size = 1000000;
+    numbers = newArrayList();
+    for (int i = 0; i < size; i++) {
+      numbers.add((int) Math.floor(Math.random() * size));
+    }
+    numbersTrustedSort = newArrayList(numbers);
+    Collections.sort(numbersTrustedSort);
+    MergeSort.sort(numbers);
+    assertEquals(numbersTrustedSort, numbers);
+  }
+
+  @Test
+  public void sortFailTest() {
+    int size = 10;
+    numbers = newArrayList();
+    for (int i = 0; i < size; i++) {
+      numbers.add((int) Math.floor(Math.random() * size));
+    }
+    numbersTrustedSort = newArrayList(numbers);
+    MergeSort.sort(numbers);
+    assertNotEquals(numbersTrustedSort, numbers);
+  }
+}
